d590418c6061320e9428f7547a8c90a83f833e29
furkhan ar <mdfurkhanar@gmail.com>
2026-05-13 15:02:23 +0530

    Configured Railway database

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,20 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "furkh@n@r06",
-    database: "taskmanager"
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-connection.connect((err) => {
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log("MySQL Connected");
-    }
+db.connect((err) => {
+  if(err){
+    console.log("Database connection failed", err);
+  } else {
+    console.log("MySQL Connected");
+  }
 });
 
-module.exports = connection;
+module.exports = db;
