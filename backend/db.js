6e65a6b5b8ecfeb557cbad83177de2b6e8079b98
furkhan ar <mdfurkhanar@gmail.com>
2026-05-13 15:20:31 +0530

    Using Railway public URL

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,11 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
-  if(err){
-    console.log("Database connection failed", err);
+  if (err) {
+    console.log("Database connection failed:", err);
   } else {
     console.log("MySQL Connected");
   }
